bed496bccfb803975fb71269c6e5176fed2e07af
Patrik <74565970+PatSwag69@users.noreply.github.com>
2021-01-09 22:53:59 +0100

    Moj ZD
    LoseFocus za x Y i Z os
    P.S. možda kasnije nadodat u skripti
    void Start ()
    {
    MaxAngle = 15;
    }

---
+++ Assets/Scripts/LoseFocus.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseFocus : MonoBehaviour
+{     // by Patrik
+    public float MaxAngle;
+
+    [SerializeField]
+    public Transform xAxis;
+    public Transform yAxis;
+    public Transform zAxis;
+
+    public Transform Cam;
+
+    [SerializeField]
+    public float _angleX;
+    public float _angleY;
+    public float _angleZ;
+
+    private Vector3 _CM;
+
+    private void LateUpdate()
+    {
+        #region X-os
+        _CM = Cam.position - xAxis.position;
+        _angleX = Vector3.Angle(xAxis.right, _CM);
+
+        if (_angleX > 90)
+        {
+            _angleX = 180 - _angleX;
+        }
+        if (_angleX < MaxAngle)
+        {
+            xAxis.gameObject.SetActive(false);
+        }
+        else
+        {
+            xAxis.gameObject.SetActive(true);
+        }
+        #endregion
+
+        #region Y-os
+        _CM = Cam.position - yAxis.position;
+        _angleY = Vector3.Angle(yAxis.up, _CM);
+
+        if (_angleY > 90)
+        {
+            _angleY = 180 - _angleY;
+        }
+        if (_angleY < MaxAngle)
+        {
+            yAxis.gameObject.SetActive(false);
+        }
+        else
+        {
+            yAxis.gameObject.SetActive(true);
+        }
+        #endregion
+
+        #region Z-os
+
+        _CM = Cam.position - zAxis.position;
+        _angleZ = Vector3.Angle(zAxis.forward, _CM);
+
+        if (_angleZ > 90)
+        {
+            _angleZ = 180 - _angleZ;
+        }
+        if (_angleZ < MaxAngle)
+        {
+            zAxis.gameObject.SetActive(false);
+        }
+        else
+        {
+            zAxis.gameObject.SetActive(true);
+        }
+        #endregion
+
+    }
+
+}
