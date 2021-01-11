7ae6e1e976d0a05d495ac5d69ff852509104e3c0
Karlitos115 <kgajsak@tvz.hr>
2021-01-11 19:46:53 +0100

    InspectorScript update
    Poboljšan kod

--- Assets/Scripts/InspectorScript.cs
+++ Assets/Scripts/InspectorScript.cs
@@ -33,7 +33,7 @@ public class InspectorScript : MonoBehaviour
 
     private bool itemIns = false;
     private bool proxIns = false;
-    private bool laseIns = false;
+    private bool laserIns = false;
     private bool roboIns = false;
     private bool treadIns = false;
     private bool physicsToggle = true; //ignore
@@ -109,41 +109,7 @@ public class InspectorScript : MonoBehaviour
         if (interest!=null && itemIns)
         {
             SetComponents();
-            
-            //positions
-            if (!pos[0].isFocused)
-                pos[0].text = interest.transform.position.x.ToString();
-
-            if (!pos[1].isFocused)
-                pos[1].text = interest.transform.position.y.ToString();
-
-            if (!pos[2].isFocused)
-                pos[2].text = interest.transform.position.z.ToString();
-
-            //scale
-            if (!scal[0].isFocused)
-                scal[0].text = interest.transform.localScale.x.ToString();
-
-            if (!scal[1].isFocused)
-                scal[1].text = interest.transform.localScale.y.ToString();
-
-            if (!scal[2].isFocused)
-                scal[2].text = interest.transform.localScale.z.ToString();
-
-            //rotation
-            if (!rot[0].isFocused)
-                rot[0].text = interest.transform.eulerAngles.x.ToString();
-
-            if (!rot[1].isFocused)
-                rot[1].text = interest.transform.eulerAngles.y.ToString();
-
-            if (!rot[2].isFocused)
-                rot[2].text = interest.transform.eulerAngles.z.ToString();
-
-            interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
-            interest.transform.localScale = new Vector3(float.Parse(scal[0].text), float.Parse(scal[1].text), float.Parse(scal[2].text));
-            interest.transform.rotation = Quaternion.Euler(float.Parse(rot[0].text), float.Parse(rot[1].text), float.Parse(rot[2].text));
-
+            Set();
             //component
             if (!mass.isFocused)
                 mass.text = rb.mass.ToString();
@@ -160,22 +126,7 @@ public class InspectorScript : MonoBehaviour
         if (interest != null && proxIns)
         {
             SetComponents();
-
-            //positions
-            if (!pos[0].isFocused)
-                pos[0].text = interest.transform.position.x.ToString();
-
-            if (!pos[1].isFocused)
-                pos[1].text = interest.transform.position.y.ToString();
-
-            if (!pos[2].isFocused)
-                pos[2].text = interest.transform.position.z.ToString();
-
-            if (!proxyDistanceDetection.isFocused)
-                proxyDistanceDetection.text = interest.transform.GetComponent<ProxySensor_Script>().distanceDetection.ToString();
-
-            interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
-            interest.transform.rotation = Quaternion.Euler(float.Parse(rot[0].text), float.Parse(rot[1].text), float.Parse(rot[2].text));
+            Set();
 
             //component
             interest.transform.GetComponent<ProxySensor_Script>().distanceDetection = float.Parse(proxyDistanceDetection.text);
@@ -184,78 +135,42 @@ public class InspectorScript : MonoBehaviour
         if (interest != null && treadIns)
         {
             SetComponents();
-
-            //positions
-            if (!pos[0].isFocused)
-                pos[0].text = interest.transform.position.x.ToString();
-
-            if (!pos[1].isFocused)
-                pos[1].text = interest.transform.position.y.ToString();
-
-            if (!pos[2].isFocused)
-                pos[2].text = interest.transform.position.z.ToString();
-
-            if (!treadSpeed.isFocused)
-                treadSpeed.text = interest.transform.GetComponent<Treadmill_Script>().treadSpeed.ToString();
-
-            //scale
-            if (!scal[0].isFocused)
-                scal[0].text = interest.transform.localScale.x.ToString();
-
-            if (!scal[1].isFocused)
-                scal[1].text = interest.transform.localScale.y.ToString();
-
-            if (!scal[2].isFocused)
-                scal[2].text = interest.transform.localScale.z.ToString();
-
-            interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
-            interest.transform.rotation = Quaternion.Euler(float.Parse(rot[0].text), float.Parse(rot[1].text), float.Parse(rot[2].text));
-            interest.transform.localScale = new Vector3(float.Parse(scal[0].text), float.Parse(scal[1].text), float.Parse(scal[2].text));
-            
+            Set();
             //component
             interest.transform.GetComponent<Treadmill_Script>().treadSpeed = float.Parse(treadSpeed.text);
         }
 
-        if (interest != null && laseIns)
+        if (interest != null && laserIns)
         {
             SetComponents();
-
-            if (!pos[0].isFocused)
-                pos[0].text = interest.transform.position.x.ToString();
-
-            if (!pos[1].isFocused)
-                pos[1].text = interest.transform.position.y.ToString();
-
-            if (!pos[2].isFocused)
-                pos[2].text = interest.transform.position.z.ToString();
-
-            interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
-            interest.transform.rotation = Quaternion.Euler(float.Parse(rot[0].text), float.Parse(rot[1].text), float.Parse(rot[2].text));
+            Set();
         }
 
         if (interest != null && roboIns)
         {
             SetComponents();
-
-            if (!pos[0].isFocused)
-                pos[0].text = interest.transform.position.x.ToString();
-
-            if (!pos[1].isFocused)
-                pos[1].text = interest.transform.position.y.ToString();
-
-            if (!pos[2].isFocused)
-                pos[2].text = interest.transform.position.z.ToString();
-
-            interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
+            Set();
         }
 
 
+    }
+    void Set()
+    {
+        pos[0].text = interest.transform.position.x.ToString();
+        pos[1].text = interest.transform.position.y.ToString();
+        pos[2].text = interest.transform.position.z.ToString();
+        scal[0].text = interest.transform.localScale.x.ToString();
+        scal[1].text = interest.transform.localScale.y.ToString();
+        scal[2].text = interest.transform.localScale.z.ToString();
+        interest.transform.position = new Vector3(float.Parse(pos[0].text), float.Parse(pos[1].text), float.Parse(pos[2].text));
+        interest.transform.localScale = new Vector3(float.Parse(scal[0].text), float.Parse(scal[1].text), float.Parse(scal[2].text));
+        interest.transform.rotation = Quaternion.Euler(float.Parse(rot[0].text), float.Parse(rot[1].text), float.Parse(rot[2].text));
     }
 
     void SetBooleans(bool tread, bool laser, bool proximity, bool item, bool robot)
     {
         treadIns = tread;
-        laseIns = laser;
+        laserIns = laser;
         proxIns = proximity;
         itemIns = item;
         roboIns = robot;
