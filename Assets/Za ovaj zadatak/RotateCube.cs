da8ab24e7c535599f67c10a8a6dbd0ccd8e40c8e
Patrik <74565970+PatSwag69@users.noreply.github.com>
2021-03-05 22:11:45 +0100

    RješenjeodPetra
    FML

--- Assets/Za ovaj zadatak/RotateCube.cs
+++ Assets/Za ovaj zadatak/RotateCube.cs
@@ -2,39 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateCube : MonoBehaviour
+public class RotateCube : START
 {
-    public float Speed;
+    public float Speed_Rot;
 
-    bool _action = false;
+
+    void Start()
+    {
+        PutIn(transform);
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // pricekaj klik misa
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitObject;
-
-            if (Physics.Raycast(ray, out hitObject)) // pregledaj sto si lupio
-            {
-                if (hitObject.transform.gameObject.name == "RotateCube") // ako je u tom layeru onda mozes obavljat akciju
-                    _action = true;
-            }
-        }
-
-        if (Input.GetMouseButton(0) && _action)
-        {
-            //ACTION
-            float mouseX = Input.GetAxis("Mouse X");
-            if (mouseX != 0)
-            {
-                Vector3 temp = transform.localEulerAngles; //nemozemo direktno mijenjati vrijednost x osi transforma pa spremamo u temp
-                temp.x += (mouseX * Speed);
-                transform.localEulerAngles = temp;
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0)) //otpustis li mis gotovo je
-            _action = false;
-
+        NewTrans.localEulerAngles = SelectMe();
     }
 }
--- Assets/Za ovaj zadatak/ScaleCube.cs
+++ Assets/Za ovaj zadatak/ScaleCube.cs
@@ -2,39 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScaleCube : MonoBehaviour
+public class ScaleCube : START
 {
-    public float Speed;
+    public float Speed_Scal =5;
 
-    bool _action = false;
+
+    void Start()
+    {
+        PutIn(transform);
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // pricekaj klik misa
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitObject;
-
-            if (Physics.Raycast(ray, out hitObject)) // pregledaj sto si lupio
-            {
-                if (hitObject.transform.gameObject.name == "ScaleCube") // ako je u tom layeru onda mozes obavljat akciju
-                    _action = true;
-            }
-        }
-
-        if (Input.GetMouseButton(0) && _action)
-        {
-            //ACTION
-            float mouseX = Input.GetAxis("Mouse X");
-            if (mouseX != 0)
-            {
-                Vector3 temp = transform.localScale; //nemozemo direktno mijenjati vrijednost x osi transforma pa spremamo u temp
-                temp.x += (mouseX * Speed);
-                transform.localScale = temp;
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0)) //otpustis li mis gotovo je
-            _action = false;
-
+        NewTrans.localScale= SelectMe();
     }
 }
