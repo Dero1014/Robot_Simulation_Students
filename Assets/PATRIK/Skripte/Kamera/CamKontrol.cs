251f13fd2a1ac7fdcaa29fb4c7541a782d42ac20
Patrik <74565970+PatSwag69@users.noreply.github.com>
2021-03-10 22:18:33 +0100

    update
    kamera

--- Assets/PATRIK/Skripte/Kamera/CamKontrol.cs
+++ Assets/PATRIK/Skripte/Kamera/CamKontrol.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamKontrol : MonoBehaviour
-{    // ----------Zumiranje----------
+{   // ----------Zumiranje----------
     private Camera cam;
     private float targetZoom;
     private float zoomFactor = 3f;
     [SerializeField] private float zoomLerpSpeed = 10;
-    // ----------Zumiranje-----------
-
-
-
+    //------Rotacija-------
+    float speedH = 2.0f;
+    float speedV = 2.0f;
+    float yaw = 0.0f;
+    float pitch = 0.0f;
+  
+     
 
     void Start()
     {
@@ -28,8 +31,25 @@ public class CamKontrol : MonoBehaviour
         float scrollData;
         scrollData = Input.GetAxis("Mouse ScrollWheel");
         targetZoom -= scrollData * zoomFactor;
-        targetZoom = Mathf.Clamp(targetZoom, 1.5f, 20f);
+        targetZoom = Mathf.Clamp(targetZoom, 1.0f, 20f);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
-        // ----------Zumiranje-----------
+        //------Kretanje-----
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+             
+        }
+
+    }
+    private void LateUpdate()
+    {
+        //-------Rotacija------
+        if (Input.GetMouseButton(0))
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+        //------Rotacija-------
     }
 }
