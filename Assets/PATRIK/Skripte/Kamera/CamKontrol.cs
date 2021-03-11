9aabcdd65671e293348feb2ba975acf8ac3df45b
Patrik <74565970+PatSwag69@users.noreply.github.com>
2021-03-11 13:46:42 +0100

    Update CamKontrol.cs
    update

--- Assets/PATRIK/Skripte/Kamera/CamKontrol.cs
+++ Assets/PATRIK/Skripte/Kamera/CamKontrol.cs
@@ -5,51 +5,88 @@ using UnityEngine;
 public class CamKontrol : MonoBehaviour
 {   // ----------Zumiranje----------
     private Camera cam;
-    private float targetZoom;
-    private float zoomFactor = 3f;
-    [SerializeField] private float zoomLerpSpeed = 10;
+    private float startingFOV;
+
+    public float minFOV;
+    public float maxFOV;
+    public float zoomRate;
+
+    private float currentFOV;
+
     //------Rotacija-------
     float speedH = 2.0f;
     float speedV = 2.0f;
     float yaw = 0.0f;
     float pitch = 0.0f;
-  
+
+    //--------Kretanje--------
+    Vector3 tempPos;
      
 
     void Start()
     {
         // ----------Zumiranje-----------
-        cam = Camera.main;
-        targetZoom = cam.orthographicSize;
-        // ----------Zumiranje-----------
+        cam = GetComponent<Camera>();
+        startingFOV = cam.fieldOfView;
 
-    }
+    } // - Zumiranje
 
     void Update()
     {
-        // ----------Zumiranje-----------
-        float scrollData;
-        scrollData = Input.GetAxis("Mouse ScrollWheel");
-        targetZoom -= scrollData * zoomFactor;
-        targetZoom = Mathf.Clamp(targetZoom, 1.0f, 20f);
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+        // ----------Zumiranje----------
+        UseWheel();
+
         //------Kretanje-----
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-             
-        }
+        Kretanje();
 
     }
     private void LateUpdate()
     {
         //-------Rotacija------
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
             yaw += speedH * Input.GetAxis("Mouse X");
             pitch -= speedV * Input.GetAxis("Mouse Y");
 
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         }
-        //------Rotacija-------
+    } // -Rotacija
+    public void UseWheel()
+    {
+        currentFOV = cam.fieldOfView;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        currentFOV += scroll * zoomRate;
+
+        currentFOV = Mathf.Clamp(currentFOV, minFOV, maxFOV);
+        cam.fieldOfView = currentFOV;
+    }
+    public void Kretanje()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            tempPos = transform.position;
+            tempPos.x += 1f;
+            transform.position = tempPos;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            tempPos = transform.position;
+            tempPos.x -= 1f;
+            transform.position = tempPos;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            tempPos = transform.position;
+            tempPos.z += 1f;
+            transform.position = tempPos;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            tempPos = transform.position;
+            tempPos.z -= 1f;
+            transform.position = tempPos;
+        }
     }
 }
