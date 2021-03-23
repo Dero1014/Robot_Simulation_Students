f684dbc5d571e4a2b145d87adbf06c6e2f686b64
Emil-J <57983428+Emil-J@users.noreply.github.com>
2021-03-23 21:54:25 +0100

    UI_Resize02
    Dodao sam još texta. Nešto sam zaboravio, poz.

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -9,9 +9,9 @@ using UnityEngine;
 //Zatim sve anchore selektirati ih oprezno po sredini i postaviti ih sve u gornji lijevi kut za PauseButton.
 //Za SpawnPanel isto kao i u lineu 9 samo sto ih treba postaviti u donji desni kut.
 //Na kraju za MoveToolButton i InspectorPanel isto kao i u lineu 9 && 10 samo ih treba postaviti u gornji desni kut.
-//
-//
-//
+//Također skužio sam sada. Tokom igranja/podešavanja može se pomaknuti RobotConsole. To onda postavite sa plavim centriranim
+//kružićem u donji lijevi kut. i odite na rect transform i ispod rect transforma ce biti kvadraticm, klikne se na to i odaberi
+//bottom left.
 //
 //
 //
