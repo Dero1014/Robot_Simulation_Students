37b8a512730c2893590775fc1fe7d1f732e58cb5
Emil-J <57983428+Emil-J@users.noreply.github.com>
2021-03-23 21:46:50 +0100

    UI_Resize Update01
    Opisao sam što sam napravio. Mislim da je to točno, tak da provjeri kad stigneš, poz.

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//GameUI,PauseMenu,InspectorAndMovePanel,PauseUI sve njihove granice odnosno dimenzije namjestiti kao dimenziju Canvasa(pomoću plavih kružića)
+//treba jako dobro paziti da budu dobro alignani rubovi sa rubovima canvasa. Zatim sve anchore treba razdvojiti i postaviti svaki taj maleni trokutic
+//na krajeve/rubove canvasa. To se treba napraviti za GameUI,PauseMenu,InspectorAndMovePanel,PauseUI.
+//Zatim sve anchore selektirati ih oprezno po sredini i postaviti ih sve u gornji lijevi kut za PauseButton.
+//Za SpawnPanel isto kao i u lineu 9 samo sto ih treba postaviti u donji desni kut.
+//Na kraju za MoveToolButton i InspectorPanel isto kao i u lineu 9 && 10 samo ih treba postaviti u gornji desni kut.
+//
+//
+//
+//
+//
+//
