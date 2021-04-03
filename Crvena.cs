d0ead2c2f855777594a1a33bfc44f80c763ebf6d
Patrik <74565970+PatSwag69@users.noreply.github.com>
2021-04-03 19:48:23 +0200

    boop

---
+++ Crvena.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crvena : MonoBehaviour
+{
+    private GameObject target;
+    void OnMouseOver()
+    {
+        target = GameObject.FindWithTag("Crvena");
+        float xPos = target.transform.position.x;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.GetAxis("Mouse X") < xPos)
+            {
+                transform.Rotate(0, 360 * Time.deltaTime, 0);
+            }
+            if (Input.GetAxis("Mouse X")> xPos)
+            {
+                target.transform.Rotate(0, -360 * Time.deltaTime, 0);
+            }
+        }
+    }
+}
---
+++ Translation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translation : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 10f;
+    
+    private void Start()
+    {
+        gameObject.tag = "Crvenu";
+    }
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        }
+
+    }
+}  
+
